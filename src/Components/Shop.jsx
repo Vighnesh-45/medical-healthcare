190a6543378f7e8b6282490e78e2324b390a7d0f
Vighnesh-45 <dhotrevighnesh45@gmail.com>
2024-07-22 11:58:44 +0530

    shop page filter done

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -13,6 +13,12 @@ const Shop = ({ cart, addToCart, addToSingle }) => {
     const [totalPages, setTotalPages] = useState(1);
     const [sortOption, setSortOption] = useState('default');
     const [itemsPerPage, setItemsPerPage] = useState(12);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [ratingFilter, setRatingFilter] = useState('');
+    const [priceFilter, setPriceFilter] = useState({ min: '', max: '' });
+    const [genericBrandFilter, setGenericBrandFilter] = useState('');
+    const [formulationFilter, setFormulationFilter] = useState('');
+    const [companyNameFilter, setCompanyNameFilter] = useState('');
     const navigate = useNavigate();
 
     const getData = async () => {
@@ -70,6 +76,43 @@ const Shop = ({ cart, addToCart, addToSingle }) => {
         }
     };
 
+    const handleFilterButtonClick = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleModalClose = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleApplyFilters = () => {
+        setCurrentPage(1);
+        setIsModalOpen(false);
+    };
+
+    const handleRatingChange = (event) => {
+        setRatingFilter(event.target.value);
+    };
+
+    const handlePriceChange = (event) => {
+        const { name, value } = event.target;
+        setPriceFilter((prevPriceFilter) => ({
+            ...prevPriceFilter,
+            [name]: value
+        }));
+    };
+
+    const handleGenericBrandChange = (event) => {
+        setGenericBrandFilter(event.target.value);
+    };
+
+    const handleFormulationChange = (event) => {
+        setFormulationFilter(event.target.value);
+    };
+
+    const handleCompanyNameChange = (event) => {
+        setCompanyNameFilter(event.target.value);
+    };
+
     const getPaginatedData = () => {
         let sortedData = [...data];
 
@@ -77,6 +120,31 @@ const Shop = ({ cart, addToCart, addToSingle }) => {
             sortedData.sort((a, b) => a.SP - b.SP);
         } else if (sortOption === 'high') {
             sortedData.sort((a, b) => b.SP - a.SP);
+        }
+
+        if (ratingFilter) {
+            sortedData = sortedData.filter(item => item.Rating >= ratingFilter);
+        }
+
+        if (priceFilter.min || priceFilter.max) {
+            sortedData = sortedData.filter(item => {
+                const price = item.SP;
+                const min = priceFilter.min ? parseFloat(priceFilter.min) : 0;
+                const max = priceFilter.max ? parseFloat(priceFilter.max) : Infinity;
+                return price >= min && price <= max;
+            });
+        }
+
+        if (genericBrandFilter) {
+            sortedData = sortedData.filter(item => item.Type === genericBrandFilter);
+        }
+
+        if (formulationFilter) {
+            sortedData = sortedData.filter(item => item.Formulation === formulationFilter);
+        }
+
+        if (companyNameFilter) {
+            sortedData = sortedData.filter(item => item.Manufacturer.toLowerCase().includes(companyNameFilter.toLowerCase()));
         }
 
         const startIndex = (currentPage - 1) * itemsPerPage;
@@ -94,8 +162,8 @@ const Shop = ({ cart, addToCart, addToSingle }) => {
                     </h2>
                 </div>
                 <div className="shop-nav">
-                    <div className="nav-icons">
-                        <VscSettings />
+                    <div className="nav-icons" onClick={handleFilterButtonClick}>
+                        <VscSettings className='filter-icon'/>
                         <p>Filter</p>
                         <hr />
                         <p>Showing {((currentPage - 1) * itemsPerPage) + 1}–{Math.min(currentPage * itemsPerPage, data.length)} of {data.length} results</p>
@@ -150,6 +218,74 @@ const Shop = ({ cart, addToCart, addToSingle }) => {
                         </button>
                     )}
                 </div>
+                {isModalOpen && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <span className="close" onClick={handleModalClose}>&times;</span>
+                            <h2>Filters</h2>
+                            <div className="filter-options">
+                                <div className="filter-option">
+                                    <label htmlFor="rating">Rating:</label>
+                                    <select id="rating" value={ratingFilter} onChange={handleRatingChange}>
+                                        <option value="">All</option>
+                                        <option value="1">1 & up</option>
+                                        <option value="2">2 & up</option>
+                                        <option value="3">3 & up</option>
+                                        <option value="4">4 & up</option>
+                                        <option value="5">5</option>
+                                    </select>
+                                </div>
+                                <div className="filter-option">
+                                    <label htmlFor="minPrice">Min Price:</label>
+                                    <input
+                                        type="number"
+                                        id="minPrice"
+                                        name="min"
+                                        value={priceFilter.min}
+                                        onChange={handlePriceChange}
+                                    />
+                                </div>
+                                <div className="filter-option">
+                                    <label htmlFor="maxPrice">Max Price:</label>
+                                    <input
+                                        type="number"
+                                        id="maxPrice"
+                                        name="max"
+                                        value={priceFilter.max}
+                                        onChange={handlePriceChange}
+                                    />
+                                </div>
+                                <div className="filter-option">
+                                    <label htmlFor="genericBrand">Generic/Brand:</label>
+                                    <select id="genericBrand" value={genericBrandFilter} onChange={handleGenericBrandChange}>
+                                        <option value="">All</option>
+                                        <option value="generic">Generic</option>
+                                        <option value="brand">Brand</option>
+                                    </select>
+                                </div>
+                                <div className="filter-option">
+                                    <label htmlFor="formulation">Formulation:</label>
+                                    <input
+                                        type="text"
+                                        id="formulation"
+                                        value={formulationFilter}
+                                        onChange={handleFormulationChange}
+                                    />
+                                </div>
+                                <div className="filter-option">
+                                    <label htmlFor="companyName">Company Name:</label>
+                                    <input
+                                        type="text"
+                                        id="companyName"
+                                        value={companyNameFilter}
+                                        onChange={handleCompanyNameChange}
+                                    />
+                                </div>
+                            </div>
+                            <button onClick={handleApplyFilters}>Apply Filters</button>
+                        </div>
+                    </div>
+                )}
                 <Advertise />
             </div>
             <Footer />
